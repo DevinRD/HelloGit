a4304784061c4cfb25e134106b992ca36673377c
Devin <ddarnel8@msudenver.edu>
2019-01-30 23:52:51 -0700

    Test

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // Adding a comment just cause
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         System.out.println("Hello World!");
