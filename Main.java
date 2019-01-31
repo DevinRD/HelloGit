948ecdfe6ac150bd4660ef54928c5a603f6cbd98
Devin <ddarnel8@msudenver.edu>
2019-01-30 23:49:57 -0700

    Adding user input

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
         System.out.println("Hello World!");
-
+        System.out.print("Enter something: ");
+        userInput = scanner.nextLine();
+        System.out.println("You entered: " + userInput);
     }
 
 }
